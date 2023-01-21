cf70dade187174674cafad19d88fa351d396794f
Indraneel Srivastava <indraneel.max@gmail.com>
2023-01-21 21:15:21 +0000

    First pass

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cpx_monitor',
+    version='0.1.0',
+    description='A simple CPX Monitoring Tool',
+    url='https://github.com/indraneelmax/cpx_monitor',
+    author='Indraneel Srivastava',
+    author_email='indraneel.max@gmail.com',
+    packages=['cpx_monitor'],
+    install_requires=['pytest',
+                      'mock',
+                      ],
+    scripts=["bin/cpx_monitor"],
+)
