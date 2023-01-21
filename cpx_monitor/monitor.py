895b428fdea37625d4aaad63bf1959a99fbed85c
Indraneel Srivastava <indraneel.max@gmail.com>
2023-01-21 22:27:50 +0000

    add services to monitor

--- cpx_monitor/monitor.py
+++ cpx_monitor/monitor.py
@@ -2,7 +2,7 @@ import requests
 from pprint import pprint
 
 from cpx_monitor.log import get_logger
-
+from cpx_monitor.service_info import ServiceInfo
 logger = get_logger(__name__)
 
 
@@ -17,7 +17,7 @@ class CpxMonitor(object):
             server_url(str): URL to the CPX server
         """
         self._url = server_url
-        self._servers = []
+        self._services = []
 
     def fetch_servers(self):
         """
@@ -29,3 +29,27 @@ class CpxMonitor(object):
         if response.status_code == 200:
             data = response.json()
             logger.info(data)
+            self.capture_server_info(data)
+
+    def capture_server_info(self, server_ip_list):
+        """
+        Capture server info.
+
+        Args:
+            server_ip_list(list): List of IP addr of servers.
+        """
+        for server_ip in server_ip_list:
+            url = self._url + '/' + str(server_ip)
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                server_info = ServiceInfo(name=data.get(
+                    'service', 'Unknown'), ip_addr=server_ip)
+                self._services.append(server_info)
+
+    def list_services(self):
+        """
+        Print all services info.
+        """
+        for service in self._services:
+            logger.info(service)
