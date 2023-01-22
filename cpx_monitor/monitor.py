f2bcc30faa8d31ca35bbc47405c9d9b4d5e160e8
Indraneel Srivastava <indraneel.max@gmail.com>
2023-01-22 02:22:09 +0000

    use prettytable and coloring based on num hosts

--- cpx_monitor/monitor.py
+++ cpx_monitor/monitor.py
@@ -1,11 +1,40 @@
+from prettytable import PrettyTable
 import requests
-from pprint import pprint
 
 from cpx_monitor.log import get_logger
 from cpx_monitor.service_info import ServiceInfo
 logger = get_logger(__name__)
 
 NO_NAME = "Unknown"
+
+
+class ConsoleColor:
+    # Color
+    BLACK = '\033[90m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    GRAY = '\033[97m'
+
+    # Style
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    # BackgroundColor
+    BgBLACK = '\033[40m'
+    BgRED = '\033[41m'
+    BgGREEN = '\033[42m'
+    BgORANGE = '\033[43m'
+    BgBLUE = '\033[44m'
+    BgPURPLE = '\033[45m'
+    BgCYAN = '\033[46m'
+    BgGRAY = '\033[47m'
+
+    # End
+    END = '\033[0m'
 
 
 class CpxMonitor(object):
@@ -54,19 +83,31 @@ class CpxMonitor(object):
 
     def list_services_avg(self):
         """
-        Print services avg cpu/mem info .
+        Print services avg cpu/mem info.
         """
+        ptable = PrettyTable(["Service", "CPU", "MEMORY"])
         logger.info("Listing services - ")
         for name, service in self._services.items():
-            logger.info(service)
-            logger.info(
-                "{}% - {}%".format(service.avg_cpu, service.avg_memory))
+            if service.needs_attention:
+                ptable.add_row(
+                    [ConsoleColor.RED + name, service.avg_cpu, str(service.avg_memory) + ConsoleColor.END])
+            else:
+                ptable.add_row(
+                    [ConsoleColor.GREEN + name, service.avg_cpu, str(service.avg_memory) + ConsoleColor.END])
+        print(ptable)
 
     def list_services(self):
         """
         Print cpu/mem info for all hosts per service. 
         """
+        ptable = PrettyTable(
+            ["IP", "Service", "CPU", "MEMORY"])
         for name, service in self._services.items():
             for host in service.hosts:
-                logger.info("{} - {} - {}% - {}%".format(host.ip_addr,
-                            name, host.cpu, host.memory))
+                if service.needs_attention:
+                    ptable.add_row([ConsoleColor.RED + host.ip_addr,
+                                   name, host.cpu, host.memory + ConsoleColor.END])
+                else:
+                    ptable.add_row([ConsoleColor.GREEN + host.ip_addr,
+                                   name, host.cpu, host.memory + ConsoleColor.END])
+        print(ptable)
