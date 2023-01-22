13cc4b68d7395ceb69a02ffdf5cdf43b17a5a895
Indraneel Srivastava <indraneel.max@gmail.com>
2023-01-22 02:23:06 +0000

    add CustomFormatter to log

--- cpx_monitor/log.py
+++ cpx_monitor/log.py
@@ -9,12 +9,40 @@ FORMATTER = logging.Formatter(
 LOG_LEVEL = logging.DEBUG if os.getenv(LOG_DEBUG_ENV) else logging.INFO
 
 
+class CustomFormatter(logging.Formatter):
+
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    # format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    format = "%(message)s"
+    RED_FORMAT = red + format + reset
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        # log_fmt = self.FORMATS.get(record.levelno)
+        if hasattr(record, 'color'):
+            log_fmt = self.RED_FORMAT
+        else:
+            log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def get_console_handler():
     """
     Returns the console handler.
     """
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(FORMATTER)
+    console_handler.setFormatter(CustomFormatter())
     return console_handler
 
 
